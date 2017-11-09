1f5fc23dfdf50b2c83207ba48c73f397ab7f5deb
Sigit Budi Darmawan <cigetbudi@gmail.com>
2017-11-09 17:25:36 +0700

    Sigit pertama
    master Supplier, Customer jadi + validasi

---
+++ VapeStore/VapeStore.ViewModel/CustomerViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VapeStore.ViewModel
+{
+    public class CustomerViewModel
+    {
+        public int Id { get; set; }
+        
+        public string CostumerName { get; set; }
+        
+        public string Address { get; set; }
+
+       [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Only numbers allowed")]
+        [MaxLength(12, ErrorMessage = "Can't be more than 12 charracters")]
+        public string Phone { get; set; }
+        
+        public bool IsActive { get; set; }
+
+        public DateTime? Created { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime? Modified { get; set; }
+
+        public string ModifiedBy { get; set; }
+
+    }
+}
